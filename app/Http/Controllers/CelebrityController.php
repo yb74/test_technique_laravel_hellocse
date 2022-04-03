e6dd09f7c7c87ae29b1dfdb67ed30393ad05512a
Younes Bouaziz <54204783+yb74@users.noreply.github.com>
2022-04-03 12:17:04 +0200

    Update Controller

--- app/Http/Controllers/CelebrityController.php
+++ app/Http/Controllers/CelebrityController.php
@@ -1,174 +1,133 @@
-const manageDeleteMessage = () => { // function that display a message when a record is deleted with success and remove it after 5 seconds
-    $('span.js-delete-msg').html('<div class="alert alert-danger alert-dismissible fade show text-center">"The celebrity sheet has been deleted."</div>');
-    setTimeout(() => {$('span.js-delete-msg').html(" ")}, 5000);
-    $(window).scrollTop(0);
-}
+<?php
 
-const manageCreateMessage = () => { // function that display a message when a record is created with success and remove it after 5 seconds
-    $('span.js-delete-msg').html('<div class="alert alert-success alert-dismissible fade show text-center">"The celebrity sheet has been created."</div>');
-    setTimeout(() => {$('span.js-delete-msg').html(" ")}, 5000);
-    $(window).scrollTop(0);
-}
+namespace App\Http\Controllers;
 
-const manageUpdateMessage = () => { // function that display a message when a record is updated with success and remove it after 5 seconds
-    $('span.js-delete-msg').html('<div class="alert alert-warning alert-dismissible fade show text-center">"The celebrity sheet has been updated."</div>');
-    setTimeout(() => {$('span.js-delete-msg').html(" ")}, 5000);
-    $(window).scrollTop(0);
-}
+use Illuminate\Http\Request;
 
-// ############################### get the clicked celebrity sheet ################################# //
-const getClickedCelebrity = (id) => {
-    const elt = document.getElementById(id);
-    console.log("clicked Element = %o", elt);
+use App\Models\Celebrity;
 
-    $.ajax({
-        url: `celebrities/${id}`,
-        type: 'POST',
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        data: {
-            _token: $("input[name=_token]").val()
-        },
-        success: response => {
-            // $("#"+id).css("background-color", "lightgreen");
+class CelebrityController extends Controller
+{
+    /**
+        * Display a listing of the resource.
+        *
+        * @return Response
+        */
+        public function index()
+        {
+            $celebrities = Celebrity::all();
 
-            $('#displayDetails').html(response);
-        },
-        error : (request,error) => {
-            console.log("Request: %o", request);
+            return view('celebrities', [
+                'celebrities' => $celebrities
+            ]);
         }
-    });
-    $(window).scrollTop(0);
-}
 
-// ############################### Create a celebrity sheet ################################# //
+    /**
+        * Display the clicked resource.
+        *
+        * @return Response
+        */
+        public function showDetails($id)
+        {
+            $celebrities = Celebrity::all();
+            $celebrity = Celebrity::find($id);
 
-const submitCelebritySheetBtn = $("#createCelebritySheet");
+            $celebrityInfo = [
+                'image' => $celebrity->image,
+                'firstname' => $celebrity->firstname,
+                'lastname' => $celebrity->lastname,
+                'description' => $celebrity->description
+            ];
 
-submitCelebritySheetBtn.click((e) => {
-    e.preventDefault();
+            // $celebrityClicked = response()->json($celebrity)->original;
+            // $celebrityDetailsContent = "<h1>{$celebrityClicked->firstname}</h1>";
 
-    let firstnameInputVal = $('input[name="firstname"]').val();
-    let lastnameInputVal = $('input[name="lastname"]').val();
-    let descriptionInputVal = $('input[name="description"]').val();
-    let imageInputVal = $('input[name="image"]').get(0).files[0];
+            // return $celebrityDetailsContent;
 
-    console.log(imageInputVal);
+            return view('celebrities', [
+                'celebrities' => $celebrities,
+                'celebrityInfo' => $celebrityInfo,
+                // 'celebrityDetailsContent' => $celebrityDetailsContent
+            ]);
+        }
 
-    if (firstnameInputVal !== null || firstnameInputVal !== "" 
-        || lastnameInputVal !== null || lastnameInputVal !== "" 
-        || descriptionInputVal !== null || descriptionInputVal !== ""
-        || imageInputVal !== null || imageInputVal !== "" || imageInputVal !== undefined
-    ) {
-        console.log($('#createForm'));
-        let formData = new FormData();
+    /**
+        * Create a new resource.
+        *
+        * @return Response
+        */
+    public function createCelebritySheet(Request $request)
+    {
+        $celebrity = new Celebrity;
 
-        formData.append('firstname', firstnameInputVal);
-        formData.append('lastname', lastnameInputVal);
-        formData.append('description', descriptionInputVal);
-        formData.append('image', imageInputVal);
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'description' => 'required',
+            'image' => 'required|image',
+        ]);
 
-        console.log(formData);
+        $imageSize = $request->file('image')->getSize();
+        $imageName = $request->file('image')->getClientOriginalName();
 
-        $.ajax({
-            url: `celebrities`,
-            type: 'POST',
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            processData: false,
-            contentType: false,
-            data: formData,
-            success: response => {
-                console.log("Form data sent to php");
-                location.reload();
-                manageCreateMessage();
-            },
-            error : (request,error) => {
-                console.log("Request: %o", request);
-            }
-        });
-    } else {
-        alert("please, fill all the fields");
+        $pathFile = $request->file('image')->storeAs('public/upload/', $imageName);
+
+        $celebrity->firstname = $request->firstname;
+        $celebrity->lastname = $request->lastname;
+        $celebrity->description = $request->description;
+        $celebrity->image = $imageName;
+
+        $celebrity->save();
+
+        return response()->json([
+            'pathFile' => $pathFile,
+        ]);
     }
-});
 
+    /**
+        * Update the specified resource in the database.
+        *
+        * @param  int  $id
+        * @return Response
+        */
+    public function updateCelebritySheet(Request $request, $id)
+    {
+        $celebrity = Celebrity::find($id);
 
-// ############################### Update a celebrity sheet ################################# //
-const updateClickedCelebrity = (id) => {
+        $request->validate([
+            'firstname_update' => 'required',
+            'lastname_update' => 'required',
+            'description_update' => 'required',
+            'image_update' => 'required|image',
+        ]);
 
-    const updateCelebritySheetBtn = $("#updateCelebritySheet");
+        $imageUpdateSize = $request->file('image_update')->getSize();
+        $imageUpdateName = $request->file('image_update')->getClientOriginalName();
 
-    updateCelebritySheetBtn.click((e) => {
-        e.preventDefault();
+        $pathFile = $request->file('image_update')->storeAs('public/upload/', $imageUpdateName);
+ 
+        $celebrity->firstname = $request->firstname_update;
+        $celebrity->lastname = $request->lastname_update;
+        $celebrity->description = $request->description_update;
+        $celebrity->image = $imageUpdateName;
 
-        let firstnameUpdateInputVal = $('input[name="firstname_update"]').val();
-        let lastnameUpdateInputVal = $('input[name="lastname_update"]').val();
-        let descriptionUpdateInputVal = $('input[name="description_update"]').val();
-        let imageUpdateInputVal = $('input[name="image_update"]').get(0).files[0];
+        $celebrity->save();
 
-        console.log(imageUpdateInputVal);
-
-        if (firstnameUpdateInputVal !== null || firstnameUpdateInputVal != "" 
-            || lastnameUpdateInputVal !== null || lastnameUpdateInputVal != "" 
-            || descriptionUpdateInputVal !== null || descriptionUpdateInputVal != ""
-            || imageUpdateInputVal !== null || imageUpdateInputVal != "" && imageUpdateInputVal != undefined
-        ) {
-            let updateformData = new FormData();
-
-            updateformData.append('firstname_update', firstnameUpdateInputVal);
-            updateformData.append('lastname_update', lastnameUpdateInputVal);
-            updateformData.append('description_update', descriptionUpdateInputVal);
-            updateformData.append('image_update', imageUpdateInputVal);
-
-            console.log(updateformData);
-
-            $.ajax({
-                url: `celebrities/update/${id}`,
-                type: 'POST',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                processData: false,
-                contentType: false,
-                data: updateformData,
-                success: response => {
-                    console.log("Form data sent to php");
-                    console.log("id to update = %o", id);
-                    location.reload();   
-                    manageUpdateMessage();               
-                },
-                error : (request,error) => {
-                    console.log("Request: %o", request);
-                }
-            });
-        } else {
-            alert("please, fill all the fields");
-        } 
-    });
-}
-
-// ############################### delete a celebrity sheet ################################# //
-const deleteClickedCelebrity = (id) => { // Function that handle through ajax the removal of one element by sending its ID to the backkend
-    if (confirm("Voulez vous vraiment supprimer cet élément ?")) { // alert that asks for confirmation before deliting a record
-      $.ajax({
-        url: `celebrities/delete/${id}`,
-        type: 'DELETE',
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        data: {
-            _token: $("input[name=_token]").val()
-        },
-        success: response => {
-            $("#"+id).closest('.celebrity-h3').remove();
-            $("#celebrity_text_and_picture_container").remove();
-            manageDeleteMessage();
-        },
-        error : (request,error) => {
-             console.log("Request: %o", request);
-         }
-        });
+        return response()->json([
+            'pathFile' => $pathFile
+        ]);
     }
+
+     /**
+        * Delete the specified resource in the database.
+        *
+        * @param  int  $id
+        * @return Response
+        */
+        public function delete($id)
+        {
+            $celebrity = Celebrity::find($id);
+ 
+            $celebrity->delete();
+        }
 }
